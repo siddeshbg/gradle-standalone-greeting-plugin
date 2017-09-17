1c1e40fc7bff6c6bff9587290669c6ac1da8019c
siddesh.gurusiddappa <siddesh.gurusiddappa@appdynamics.com>
2017-09-17 17:31:02 +0530

    simple gradle plugin

--- README.md
+++ README.md
@@ -1,1 +1,9 @@
 # gradle-standalone-greeting-plugin
+
+## Building and publishing to Nexus
+./gradlew -b build1.gradle greeting
+
+## Using the plugin
+./gradlew -b build1.gradle greeting
+
+It calls the task 'greeting' from the above plugin
---
+++ src/main/groovy/org/gradle/GreetingPlugin.groovy
@@ -0,0 +1,11 @@
+package org.gradle
+
+import org.gradle.api.Project
+import org.gradle.api.Plugin
+
+class GreetingPlugin implements Plugin<Project> {
+    @Override
+    void apply(Project target) {
+        target.task('hello', type: GreetingTask)
+    }
+}
